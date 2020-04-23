bf66ef332ca237ce8511d172a4112f647277f89e
Aimee Fullerton <aimee.fullerton@gmail.com>
2020-04-23 10:06:44 -0700

    Add commented-out lines that allow for animating the growth potential maps.

--- code/AnimateImages.R
+++ code/AnimateImages.R
@@ -3,7 +3,9 @@
 # Mac Version
   # Requires ffmpeg to be installed already (developer tool)
   ani.path<- paste0(getwd(),"/",outputDir, "/", "images")
+  #ani.path = paste0(ani.path, "/GrwPot")
   dir(ani.path)
   setwd(ani.path)
   system("ffmpeg -framerate 5 -pattern_type glob -i '*.png' -c:v libxvid -b:v 2400k 1Fish.ani.mp4") #other libraries: libx264, libx265, postproc
   setwd("../../../")
+  #setwd("../")
